d8c656758f25a367fdca75b3d0df9c992282fdeb
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-08-01 12:41:28 -0500

    Set up controllers

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,10 +5,11 @@ class Ability
     user ||= User.new
     can :manage, :all
     can :manage, :all if user.role == 'admin'
-      return unless user.role == 'user'
-      can :destroy, Recipe, user_id: user.id
-      can :destroy, Food, user_id: user.id
-      can :create, Recipe
-      can :create, Food, user_id: user.id 
+    return unless user.role == 'user'
+
+    can :destroy, Recipe, user_id: user.id
+    can :destroy, Food, user_id: user.id
+    can :create, Recipe
+    can :create, Food, user_id: user.id
   end
 end
