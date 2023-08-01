08f9425f838e0c6d4d3d81b47337285420122b3f
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-01 12:27:51 -0400

    Update ability Add role to db

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,19 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user = User.new 
-    can :read, :all
-
-    if User.role == 'admin'
-      can :manage, :all
-    end
-    
-    if User.role == 'defaultUser'
-
+    user ||= User.new
+    can :manage, :all
+    can :manage, :all if user.role == 'admin'
+      return unless user.role == 'user'
       can :destroy, Recipe, user_id: user.id
       can :destroy, Food, user_id: user.id
       can :create, Recipe
-      can :create, Food, user_id: user.id
-    end 
+      can :create, Food, user_id: user.id 
   end
 end
