8ecd7e11d776828c0921d6cc0d32c26a3a7836dd
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-01 11:49:11 -0400

    Setup ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,19 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user || User.new
-    can :manage, :all
+    user = User.new 
+    can :read, :all
+
+    if User.role == 'admin'
+      can :manage, :all
+    end
+    
+    if User.role == 'defaultUser'
+
+      can :destroy, Recipe, user_id: user.id
+      can :destroy, Food, user_id: user.id
+      can :create, Recipe
+      can :create, Food, user_id: user.id
+    end 
   end
 end
