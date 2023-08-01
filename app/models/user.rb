6833a0949cda0b0870ef3c1f62e719240f57f25d
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-01 17:05:14 -0400

    Add validates for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  attribute :email, :string
+  validates :name, presence: true, allow_blank: true
+
   has_many :inventories
   has_many :recipes
 end
