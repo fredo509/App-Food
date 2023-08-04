75312d23ab3e6d82fd84b2c84e220674d0ab2d09
Daniela Moreno <dani9292@hotmail.com>
2023-08-03 23:15:48 -0500

    show inventories tests

---
+++ spec/system/inventories_show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "Inventory details page", type: :system do
+  let(:user_attributes) { { name: 'user B', email: 'test@gmail.com', password: '12345' } }
+  let(:user) { User.create(user_attributes) }
+  let(:food) { Food.create(name: 'Food 1') }
+  let(:inventory) { Inventory.create(user_id: user.id, name: 'inventory 1', description: 'Vel turpis nunc') }
+
+  before do
+    user.confirm
+    sign_in user
+
+    FoodInventory.create(inventory_id: inventory.id, food_id: food.id, quantity: 10)
+  end
+
+  it 'displays inventory details' do
+    visit user_inventory_path(user, inventory)
+
+    expect(page).to have_content('details of inventory')
+    expect(page).to have_content('inventory 1')
+    expect(page).to have_content('Inventory Details')
+    expect(page).to have_content('Food')
+   
+  end
+end
