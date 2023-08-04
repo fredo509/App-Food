8680fbad6a2a960a444de49587a1735b34959158
Daniela Moreno <dani9292@hotmail.com>
2023-08-04 03:26:14 -0500

    add tests for recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,7 +8,7 @@ class Recipe < ApplicationRecord
   validates :description, presence: true
   validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :public, inclusion: { in: [true, false] }
+  
 
   def public?
     public
